917b388b0549810903946664a61c9b313b2e9fad
Shamil Gadelshin <shamilgadelshin@gmail.com>
2023-03-23 20:56:27 +0700

    swarm: Downgrade connection limit warning.

--- swarm/src/lib.rs
+++ swarm/src/lib.rs
@@ -961,7 +961,7 @@ where
                                 error: &error,
                                 connection_id,
                             }));
-                        log::warn!("Incoming connection rejected: {:?}", connection_limit);
+                        log::debug!("Incoming connection rejected: {:?}", connection_limit);
                     }
                 };
             }
